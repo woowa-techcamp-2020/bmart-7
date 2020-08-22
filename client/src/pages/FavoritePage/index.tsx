e251ca011c8d5dc8d2ec73a3a98d56cf36619c12
Choibumsu <bumsu0211@gmail.com>
2020-08-22 23:08:43 +0900

    [update][#83] App 로드될 때 store에 데이터 fetching 수행
    - 찜한 상품 목록을 앱이 시작될 때 불러오기 위해 App에서 데이터 fetching 작업
    - store에 InitStore 컴포넌트를 작성하여, 데이터 패칭만 수행하도록 함
    - InitStore에서 데이터가 다 받아질 때까지 기다린 후, 페이지 로드

--- client/src/pages/FavoritePage/index.tsx
+++ client/src/pages/FavoritePage/index.tsx
@@ -1,36 +1,22 @@
-import React, { useEffect, useState, useContext } from 'react'
-import { client } from '@/ApolloClient'
+import React, { useContext, useEffect, useState } from 'react'
 import { ProductList } from '@/components/common/ProductList'
 import './style.scss'
-import { GET_FAVORITES } from './gql'
-import { StoreContext, SetStoreContext } from '@/store'
-import { useQuery } from 'react-apollo'
+import { StoreContext } from '@/store'
+import { FavoriteHeader } from '@/components/FavoritePage/FavoriteHeader'
 
 const COLUMN_NUM = 2
 
 export const FavoritePage = () => {
   const store = useContext(StoreContext)
-  const setStore = useContext(SetStoreContext)
-  const productList = store.favorites.map((favorite) => favorite.product)
-
-  const { loading, error, data } = useQuery(GET_FAVORITES, {
-    variables: {
-      userId: 5,
-    },
-    fetchPolicy: 'cache-and-network',
-  })
+  const [productList, setProductList] = useState([])
 
   useEffect(() => {
-    if (data) {
-      const newStore = { ...store }
-      newStore.favorites = data.getUserFavorites
-
-      setStore(newStore)
-    }
-  }, [loading, error, data])
+    setProductList(store.favorites.map((favorite) => favorite.product))
+  }, [])
 
   return (
     <div id="favorite-page">
+      <FavoriteHeader count={store.favorites.length} />
       <ProductList productList={productList} column={COLUMN_NUM} />
     </div>
   )
