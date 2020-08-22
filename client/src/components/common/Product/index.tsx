ee322d99347fa0482837c1ec94175928cb667fbd
Choibumsu <bumsu0211@gmail.com>
2020-08-22 23:18:33 +0900

    [update][#83] Product 컴포넌트 분할 및 useMutation 사용
    - Product 컴포넌트 안에 작성되어 있던 HeartIcon ,CartIcon, Price 컴포넌트 파일로 분할
    - useMutation 훅을 사용하여 API 요청
    - API 요청중인 상태에서는 찜하기를 변경할 수 없도록 수정

--- client/src/components/common/Product/index.tsx
+++ client/src/components/common/Product/index.tsx
@@ -1,97 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React from 'react'
 import './style.scss'
 import { Product as ProductType } from '@/types'
-import { IoMdHeartEmpty, IoMdHeart } from 'react-icons/io'
-import { FaShoppingCart } from 'react-icons/fa'
-import { GrCart } from 'react-icons/gr'
-import { formatPrice } from '@/utils'
-import { client } from '@/ApolloClient'
-import { INSERT_FAVORITES, DELETE_FAVORITES } from './gql'
-import { StoreContext, SetStoreContext } from '@/store'
+import { HeartIcon } from './HeartIcon'
+import { CartIcon } from './CartIcon'
+import { Price } from './Price'
 
 const imageBaseUrl = process.env.REACT_APP_S3_URL
-
-type PriceProps = {
-  originPrice: number
-  salePrice: number
-  salePercent: number
-}
-
-const Price: React.FC<PriceProps> = (props) => {
-  const { originPrice, salePrice, salePercent } = props
-
-  return (
-    <div className="price">
-      {salePercent !== 0 ? (
-        <>
-          <div className="origin-price-wrapper">
-            <div className="sale-percent">{formatPrice(salePercent)}%</div>
-            <div className="origin-price">{formatPrice(originPrice)}원</div>
-          </div>
-          <div className="display-price">{formatPrice(salePrice)}원</div>
-        </>
-      ) : (
-        <div className="display-price">{formatPrice(originPrice)}원</div>
-      )}
-    </div>
-  )
-}
-
-const HeartIcon: React.FC<{ id: number }> = ({ id }) => {
-  const store = useContext(StoreContext)
-  const setStore = useContext(SetStoreContext)
-  const [favoriteId, setFavoriteId] = useState(0)
-
-  useEffect(() => {
-    const favoriteItem = store.favorites.find((favorite) => favorite.product.id === id)
-    if (favoriteItem) setFavoriteId(favoriteItem.id)
-  }, [])
-
-  const insertFavorite = async () => {
-    const {
-      data: { insertFavorite },
-    } = await client.mutate({ mutation: INSERT_FAVORITES(5, id) })
-
-    const newStore = { ...store }
-    newStore.favorites = [...newStore.favorites, insertFavorite]
-
-    setStore(newStore)
-    setFavoriteId(insertFavorite.id)
-  }
-
-  const deleteFavorite = async () => {
-    await client.mutate({ mutation: DELETE_FAVORITES(favoriteId) })
-
-    const newStore = { ...store }
-    newStore.favorites = newStore.favorites.filter((favorite) => favorite.id !== favoriteId)
-
-    setStore(newStore)
-  }
-
-  const toggleFavorite = () => {
-    favoriteId ? deleteFavorite() : insertFavorite()
-  }
-
-  return (
-    <div className="heart icon-wrapper" onClick={toggleFavorite}>
-      <IoMdHeartEmpty className={'icon ' + (favoriteId ? 'dp-none' : '')} />
-      <IoMdHeart className={'icon ' + (!favoriteId ? 'dp-none' : '')} />
-    </div>
-  )
-}
-
-const CartIcon: React.FC<{ id: number }> = ({ id }) => {
-  const store = useContext(StoreContext)
-  const setStore = useContext(SetStoreContext)
-  //id가 카트 안에 있는지 검사 (전역 스토어에서 찾기)
-  const isInCart = false
-
-  return (
-    <div className={'cart icon-wrapper ' + (isInCart ? 'active' : '')}>
-      {isInCart ? <FaShoppingCart className="icon" /> : <GrCart className="icon" />}
-    </div>
-  )
-}
 
 export type ProductProps = {
   product: ProductType
