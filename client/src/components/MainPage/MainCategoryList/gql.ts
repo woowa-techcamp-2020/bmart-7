b5bea02c1997fa56011f885dc4b4cd2589638bf1
blair B <blair441035@gmail.com>
2020-08-23 11:12:34 +0900

    [feat][#71]MainCategory gql생성

---
+++ client/src/components/MainPage/MainCategoryList/gql.ts
@@ -0,0 +1,16 @@
+import gql from 'graphql-tag'
+
+// export const GET_RECOMMENDED = gql`
+//   query($categoryId: Int!, $offset: Int!, $limit: Int!) {
+//     getRecommended(categoryId: $categoryId, offset: $offset, limit: $limit) {
+//       id
+//       title
+//       originPrice
+//       salePrice
+//       salePercent
+//       amount
+//       mainImage
+//       bannerImage
+//     }
+//   }
+// `
