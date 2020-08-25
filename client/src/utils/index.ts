6ed43380b7e10c6211e54c0eaae205f7720bb9ec
parkjihwanjay <cc6656@maver.com>
2020-08-25 10:23:50 +0900

    [feat][#113] makeIntersectionObserver 함수 생성
    - utils에 추가
    - 특정 elemenet가 화면에 보이면 loadData 실행
    - loadData는 해당 함수를 사용하는 쪽에서 전달
    - 감지가 되면 observe 해제

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -4,3 +4,26 @@ export function formatPrice(amount) {
     .replace(/\D/g, '')
     .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
+
+const rootMargin: string = '10px'
+const threshold: number = 0.1
+
+export const makeIntersectionObserver = (loadData) => {
+  return new IntersectionObserver(
+    (entries, observer) => {
+      entries.map((entry) => {
+        if (!entry.isIntersecting) {
+          return
+        }
+        const target = entry.target
+        console.log(target)
+        loadData()
+        observer.unobserve(target)
+      })
+    },
+    {
+      threshold,
+      rootMargin,
+    }
+  )
+}
