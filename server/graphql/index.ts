9dfa424b1bb582d91c2a82080d614bc1af35bd12
Choibumsu <bumsu0211@gmail.com>
2020-08-25 16:01:44 +0900

    [feat][#115] CartItem API 작성
    - getUserCartItems : 유저의 CartItem들을 조회
    - insertCartItem : CartItem을 추가
    - putCartItemCount : CartItem의 count 컬럼을 수정
    - deleteCartItems : 인자로 넘겨준 id 리스트에 해당하는 CartItem을 모두 제거

--- server/graphql/index.ts
+++ server/graphql/index.ts
@@ -1,11 +1,12 @@
 import { loadFilesSync, mergeTypeDefs, mergeResolvers, makeExecutableSchema } from 'graphql-tools'
 import path from 'path'
 import { userResolver } from './user/user-resolver'
+import { cartResolver } from './cart/cart-resolver'
 import { categoryResolver } from './category/category-resolver'
 import { productResolver } from './product/product-resolver'
 
 const allTypes = loadFilesSync(path.join(__dirname, '/**/*.graphql'))
-const resolvers = mergeResolvers([userResolver, categoryResolver, productResolver])
+const resolvers = mergeResolvers([userResolver, cartResolver, categoryResolver, productResolver])
 
 export const schema = makeExecutableSchema({
   resolvers,
