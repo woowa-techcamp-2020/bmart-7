10cd4b6befb4e4a8e49ade2bc9ed0d30d320a8f1
parkjihwanjay <cc6656@maver.com>
2020-08-25 10:41:24 +0900

    [refactoring][#113] utils 수정
    - utils의 있는 함수들을 여러 파일로 나누고 index에서 export하는 형식으로 수정

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,29 +1,3 @@
-export function formatPrice(amount) {
-  return amount
-    .toString()
-    .replace(/\D/g, '')
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-}
-
-const rootMargin: string = '10px'
-const threshold: number = 0.1
-
-export const makeIntersectionObserver = (loadData) => {
-  return new IntersectionObserver(
-    (entries, observer) => {
-      entries.map((entry) => {
-        if (!entry.isIntersecting) {
-          return
-        }
-        const target = entry.target
-        console.log(target)
-        loadData()
-        observer.unobserve(target)
-      })
-    },
-    {
-      threshold,
-      rootMargin,
-    }
-  )
-}
+export { fetchQuery } from './fetchQuery'
+export { formatPrice } from './formatPrice'
+export { makeIntersectionObserver } from './intersectionOberserver'
