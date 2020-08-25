9dca3b586063b6beab7a181126863bf3d873b609
Choibumsu <bumsu0211@gmail.com>
2020-08-25 13:02:10 +0900

    [feat][#115] graphql-tools 만 이용해서 typedef, resolver 병합
    - 불필요한 패키지 삭제

--- server/graphql/index.ts
+++ server/graphql/index.ts
@@ -1,15 +1,13 @@
-import { makeExecutableSchema } from 'graphql-tools'
-import { mergeResolvers } from '@graphql-tools/merge'
-import { fileLoader, mergeTypes } from 'merge-graphql-schemas'
+import { loadFilesSync, mergeTypeDefs, mergeResolvers, makeExecutableSchema } from 'graphql-tools'
 import path from 'path'
 import { userResolver } from './user/user-resolver'
 import { categoryResolver } from './category/category-resolver'
 import { productResolver } from './product/product-resolver'
 
-const allTypes = fileLoader(path.join(__dirname, '/**/*.graphql'))
+const allTypes = loadFilesSync(path.join(__dirname, '/**/*.graphql'))
 const resolvers = mergeResolvers([userResolver, categoryResolver, productResolver])
 
 export const schema = makeExecutableSchema({
   resolvers,
-  typeDefs: mergeTypes(allTypes),
+  typeDefs: mergeTypeDefs(allTypes),
 })
