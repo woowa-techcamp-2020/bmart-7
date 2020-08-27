43fcb7b4175f594fe422050b9e2bff115b27303d
Choibumsu <bumsu0211@gmail.com>
2020-08-27 15:05:36 +0900

    [update][#137] MainCategoryItem 컴포넌트 수정
    - title도 props로 받음
    - s3 이미지에 연결
    - Link 추가

--- client/src/components/MainPage/MainCategoryList/MainCategoryItem/index.tsx
+++ client/src/components/MainPage/MainCategoryList/MainCategoryItem/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './style.scss'
 
 interface IconProps {
   id: number
   url: string
+  title: string
 }
 
 export const MainCategoryItem: React.FC<IconProps> = (props) => {
-  const { id, url } = props
-  const clickMainIcon = (e: any) => {
-    // todo:클릭시 해당 페이지 이동
-    console.log(e.target)
-  }
-  return <img className="category-item" data-id={id} src={url} alt="cat" onClick={clickMainIcon} />
+  const { id, url, title } = props
+  return (
+    <Link to={`/main/category/${id}`} className="category-item">
+      <img data-id={id} src={url} alt="cat" />
+      <div className="title">{title}</div>
+    </Link>
+  )
 }
