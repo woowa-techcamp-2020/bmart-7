b2c4f9d938e73b6858e8269a19785ebc27e028d5
parkjihwanjay <cc6656@maver.com>
2020-08-27 21:31:42 +0900

    [feat][#149] RandomRecommend 작성
    - 땡겨요 화면
    - setTimeout을 이용해 땡겨요 기능 구현
    - props로 reset하는 함수 받음

---
+++ client/src/pages/MainPage/RandomRecommend.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import './style.scss'
+
+interface IProps {
+  resetHandler: () => void
+}
+const menuList = ['짜장면', '탕수육', '김치찌개', '치킨']
+
+export const RandomRecommend: React.FC<IProps> = (props) => {
+  const { resetHandler } = props
+  const [menu, setMenu] = useState('암거나')
+  let idxList = [0, 1, 2, 3]
+  const getRandomIdx = (length) => {
+    return Math.floor((Math.random() * 10) % length)
+  }
+  useEffect(() => {
+    menuList.forEach((menu, idx) => {
+      const randomIdx = idxList[getRandomIdx(idxList.length)]
+      idxList = [...idxList].filter((idx) => {
+        return randomIdx !== idx
+      })
+      setTimeout(() => {
+        setMenu(menuList[randomIdx])
+      }, (400 + idx * 100) * idx)
+    })
+    setTimeout(() => {
+      resetHandler()
+    }, 2500)
+  }, [])
+
+  return <div className="recommend-container">{menu} 땡겨요</div>
+}
