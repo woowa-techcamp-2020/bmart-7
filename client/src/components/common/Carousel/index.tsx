ac7ff8a7f0ba6a7aa27975f1210209a1eb6d5f01
blair B <blair441035@gmail.com>
2020-08-27 22:52:27 +0900

    [feat][#153]change banner img

--- client/src/components/common/Carousel/index.tsx
+++ client/src/components/common/Carousel/index.tsx
@@ -5,10 +5,10 @@ import Slider from 'react-slick'
 import './style.scss'
 
 const imgUrlArr: Array<string> = [
-  'https://media.vlpt.us/images/blair/post/83bc5185-50cd-4814-8d68-77ae5f56e3c3/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA%202020-08-21%20%E1%84%8B%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%AB%2012.33.49.png',
-  'https://menu.mt.co.kr/moneyweek/thumb/2020/04/13/06/2020041315148091718_1.jpg',
-  'https://photo.jtbc.joins.com/news/2018/01/22/20180122172000506.jpg',
-  'https://menu.mt.co.kr/moneyweek/thumb/2020/04/09/06/2020040910318013214_1.jpg',
+  './images/carousel1.png',
+  './images/carousel2.png',
+  './images/carousel3.png',
+  './images/carousel4.png',
 ]
 
 export const SlickCarousel: React.FC = () => {
