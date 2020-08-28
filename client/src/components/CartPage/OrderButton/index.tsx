c99a72ba12da6402507a82b8c56deb65406fb11c
Choibumsu <bumsu0211@gmail.com>
2020-08-28 11:59:06 +0900

    [feat][#145] 주문 버튼 클릭시 주문 객체 생성

--- client/src/components/CartPage/OrderButton/index.tsx
+++ client/src/components/CartPage/OrderButton/index.tsx
@@ -1,19 +1,39 @@
 import React, { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 import './style.scss'
 import { formatPrice } from '@/utils'
 import { CartItemsContext, SetCartItemsContext } from '@/pages/CartPage'
+import { useMutation } from 'react-apollo'
+import { INSERT_ORDER } from './gql'
 
 type OrderButtonType = {
   totalPrice: number
 }
 
 export const OrderButton: React.FC<OrderButtonType> = ({ totalPrice }) => {
+  const history = useHistory()
   const cartItems = useContext(CartItemsContext)
   const setCartItems = useContext(SetCartItemsContext)
+  const [createOrder] = useMutation(INSERT_ORDER)
   const count = cartItems.filter((item) => item.isSelected).length
 
-  const orderItems = () => {
-    alert('준비중인 기능입니다.')
+  const orderItems = async () => {
+    const userId = +localStorage.getItem('userId')
+    const selectedCartItems = cartItems.reduce((idList, item) => {
+      if (item.isSelected) idList.push(item.id)
+      return idList
+    }, [])
+    await createOrder({
+      variables: {
+        input: {
+          userId,
+          cartItemIds: selectedCartItems,
+        },
+      },
+    })
+
+    alert('주문이 완료되었습니다.')
+    history.push('/order')
   }
 
   return (
